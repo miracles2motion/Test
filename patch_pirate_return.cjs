d902827f05ac229afee298ec441c373c94c5857a
Doodle Dev <dev@example.com>
2026-09-14 01:37:59 +0100

    chore(cleanup): purge obsolete one-off patch scripts, scratch files, and duplicates

--- patch_pirate_return.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/levels/pirate_cove.js', 'utf8');
-code = code.replace("B.finish();\n}", "B.finish();\n  return L;\n}");
-fs.writeFileSync('src/levels/pirate_cove.js', code);
